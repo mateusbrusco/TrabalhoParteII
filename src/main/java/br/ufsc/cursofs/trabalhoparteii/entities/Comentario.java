6c8cb6f5d43941a4d6fc2a107a16323a0eb9dbce
mateusbrusco <mateusbrusco@gmail.com>
2022-09-24 18:42:11 -0300

    Adição da entidade Comentario

---
+++ src/main/java/br/ufsc/cursofs/trabalhoparteii/entities/Comentario.java
@@ -0,0 +1,83 @@
+package br.ufsc.cursofs.trabalhoparteii.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comentario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Date data;
+	private String text;
+
+	public Comentario() {
+	}
+
+	public Comentario(Long id, String nome, Date data, String text) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.data = data;
+		this.text = text;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comentario other = (Comentario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
